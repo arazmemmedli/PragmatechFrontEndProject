8d4858abf768dd2403b765c92a00b4ff4ef8421b
Azik Memmedli <a.memmedli111@gmail.com>
2021-05-26 01:30:51 +0400

    Services, Hero container leading, Hero container mobile and Testimonials added

--- algorithm/Pre-intermediate level/main.js
+++ algorithm/Pre-intermediate level/main.js
@@ -2,3 +2,9 @@
 const arr = [678,"Freelance", [99,21, [99,12,456,["Simon","Holly"],78,34] ,45,67],45,67,89,"Raymond",23,true,"Michael","Joseph",545,188]
 
 // Question 1
+function sum(){
+    // for(let i=0;i < arr.length;i++){
+    //     console.log(arr[i])
+    // }
+}
+sum()
