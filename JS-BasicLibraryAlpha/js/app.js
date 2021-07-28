542254fb13ba7e63a354b7d980604ab74301ff12
Araz <76637491+arazmemmedli@users.noreply.github.com>
2021-07-28 21:28:08 +0400

    creatGridLayout function update olundu

--- JS-BasicLibraryAlpha/js/app.js
+++ JS-BasicLibraryAlpha/js/app.js
@@ -5,20 +5,18 @@ let setting={
 }
   
 
-function creatGridLayout() {
+function creatGridLayout(object) {
     n = setting.elementSayi;
     let row = document.createElement('div');
     row.className = 'row';
     document.body.appendChild(row)
-    m = setting.sutunSayi
-    row.style.gridTemplateColumns = `repeat(${m},1fr)`
-    a = setting.sutunlarArasiMesafe
-    row.style.gridGap = `${a}px`
-    for (let i = 0; i < n; i++) {
+    row.style.gridTemplateColumns = `repeat(${object.sutunSayi},1fr)`
+    row.style.gridGap = `${object.sutunlarArasiMesafe}px`
+    for (let i = 0; i < object.elementSayi; i++) {
         let grid = document.createElement('div');
         grid.className = 'grid__item'
         grid.style.backgroundImage = "url('3.jpg')"
         row.appendChild(grid)
     }
 }
-creatGridLayout()
+creatGridLayout(setting)
